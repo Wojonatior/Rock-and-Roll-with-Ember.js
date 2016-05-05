3e4bc97604f11e3aebafb2c592b12c2d9a4675ba
Jarek Wojciechowski <jarekwojo@gmail.com>
2016-05-05 15:57:28 -0400

    progress through chapter 12

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -5,7 +5,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	// Properties
 	title: '',
+	searchTerm: '',
 	songCreationStarted: false,
+	sortBy: 'ratingDesc',
+	queryParams: {
+		sortBy: 'sort',
+		searchTerm: 's',
+	},
 
 	// Actions
 	actions: {
@@ -23,6 +29,10 @@ export default Ember.Controller.extend({
 		enableSongCreation: function() {
 			this.set('songCreationStarted', true);
 		},
+		
+		setSorting: function(option) {
+			this.set('sortBy', option);
+		}
 	},
 
 	// Computed Properties
@@ -32,5 +42,24 @@ export default Ember.Controller.extend({
 
 	canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
 		return this.get('songCreationStarted') || this.get('model.songs.length');
+	}),
+
+	sortProperties: Ember.computed('sortBy', function() {
+		var options = {
+			'ratingDesc': 'rating:desc,title:asc',
+			'ratingAsc': 'rating:asc,title:asc',
+			'titleDesc': 'title:desc',
+			'titleAsc': 'title:asc',
+		};
+		return options[this.get('sortBy')].split(',');
+	}),
+
+	sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+	matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+		var searchTerm = this.get('searchTerm').toLowerCase();
+		return this.get('model.songs').filter(function(song) {
+			return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+		});
 	}),
 });
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params) {
-		return this.storefindRecord('band', params.id);
+		return this.store.findRecord('band', params.id);
 	},
 
 	afterModel: function(band) {
